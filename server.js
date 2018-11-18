4f3b966b0eb0d37c6b2c760a058497e97fcd19ed
Hannah Smyth-Osbourne <hannah.smyth-osbourne_ext@scout24.com>
2018-11-18 15:35:58 +0100

    Add lint for es6

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const http = require('http');
 
-const hostname = "127.0.0.1";
-const port = "3000";
+const hostname = '127.0.0.1';
+const port = '3000';
 
 
-const server = http.createServer((req, res)=> {
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Response content')
+  res.end('Response content');
 });
 
-server.listen(port,hostname,()=>{
-    console.log(`Serving running at http://${hostname}:${port}/`);
+server.listen(port,hostname,() => {
+  // eslint-disable-next-line no-console
+  console.log(`Serving running at http://${hostname}:${port}/`);
 });
