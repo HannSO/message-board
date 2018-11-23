3d4b5af3e35ff23a07717e44258658790b6e5587
Hannah Smyth-Osbourne <hannah.smyth-osbourne_ext@scout24.com>
2018-11-23 23:16:15 +0100

    Create /GET for messages

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,22 @@
 const http = require('http');
-
 const hostname = '127.0.0.1';
 const port = '3000';
 
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Response content');
-});
+const server = (repository = {}) => {
+  {
+    http.createServer((req, res) => {
+      if (req.url === '/messages' && req.method === 'GET') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(repository));
+      }
+    }).listen(port, hostname, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Serving running at http://${hostname}:${port}/`);
+    });
+  }
+};
 
-server.listen(port,hostname,() => {
-  // eslint-disable-next-line no-console
-  console.log(`Serving running at http://${hostname}:${port}/`);
-});
+
+
+module.exports = server;
---
+++ test/server/server.integration.spec.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const expect = require('chai').expect;
+const server = require('../../src/server/server');
+
+
+describe('server', () => {
+  server({messageOne: 'Super interesting', messageTwo: 'Top secret!'});
+  chai.use(chaiHttp);
+
+  it('gets saved messages', (done) => {
+    chai.request('http://localhost:3000')
+      .get('/messages')
+      .end((err, res) => {
+        if (err) {
+          expect(false);
+        }
+        expect(res.body).to.deep.equal({messageOne: 'Super interesting', messageTwo: 'Top secret!'});
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+});
