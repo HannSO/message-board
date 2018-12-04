75d2964b8d557ea9beab96893f41ae297ab4ebbc
Hannah Smyth-Osbourne <hannah.smyth-osbourne_ext@scout24.com>
2018-12-04 22:25:24 +0100

    Create /POST endpoint for message

--- src/server/server.js
+++ src/server/server.js
@@ -2,21 +2,43 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = '3000';
 
-
 const server = (repository = {}) => {
-  {
-    http.createServer((req, res) => {
-      if (req.url === '/messages' && req.method === 'GET') {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(repository));
-      }
-    }).listen(port, hostname, () => {
-      // eslint-disable-next-line no-console
-      console.log(`Serving running at http://${hostname}:${port}/`);
-    });
-  }
-};
 
+  http.createServer((req, res) => {
+
+    if (req.url === '/messages' && req.method === 'GET') {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+
+      res.end(JSON.stringify(repository));
+    }
+    if (req.url === '/message' && req.method === 'POST') {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on('end', () => {
+        const parsedBody = JSON.parse(body);
+
+        const messageBody = parsedBody.message;
+        const messageId = Object.keys(repository).length + 1;
+
+        repository = {...repository, ...{[messageId]: messageBody}};
+
+        const jsonifiedObject = JSON.stringify({[messageId]: messageBody});
+        res.end(jsonifiedObject);
+      });
+    }
+
+  }).listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Serving running at http://${hostname}:${port}/`);
+  });
+
+};
 
 
 module.exports = server;
--- test/server/server.integration.spec.js
+++ test/server/server.integration.spec.js
@@ -10,6 +10,7 @@ describe('server', () => {
   server({messageOne: 'Super interesting', messageTwo: 'Top secret!'});
   chai.use(chaiHttp);
 
+
   it('gets saved messages', (done) => {
     chai.request('http://localhost:3000')
       .get('/messages')
@@ -21,6 +22,22 @@ describe('server', () => {
         expect(res).to.have.status(200);
         done();
       });
+
+  });
+
+  it('posts message', (done) => {
+    chai.request('http://localhost:3000')
+      .post('/message')
+      .send({message: 'Third Message!'})
+      .end((err, res) => {
+        if (err) {
+          expect(false);
+        }
+        expect(res.body).to.deep.equal({3: 'Third Message!'});
+        expect(res).to.have.status(200);
+        done();
+      });
+
   });
 
 });
